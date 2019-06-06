f6016ce42898d7aa50cffc0c071813baa77fcd80
ammar ammar <ammarvega@hotmail.com>
2019-06-06 19:20:34 +0300

    Add project files.

---
+++ KnightAlgorithem/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace KnightAlgorithem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Solvebtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Sizetxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.Solve2btn = new System.Windows.Forms.Button();
+            this.Thinklbl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Solvebtn
+            // 
+            this.Solvebtn.Location = new System.Drawing.Point(69, 105);
+            this.Solvebtn.Name = "Solvebtn";
+            this.Solvebtn.Size = new System.Drawing.Size(75, 23);
+            this.Solvebtn.TabIndex = 2;
+            this.Solvebtn.Text = "Solve";
+            this.Solvebtn.UseVisualStyleBackColor = true;
+            this.Solvebtn.Click += new System.EventHandler(this.Solvebtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Row :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Clolumns :";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(204, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 390);
+            this.panel1.TabIndex = 5;
+            // 
+            // Sizetxt
+            // 
+            this.Sizetxt.Location = new System.Drawing.Point(88, 30);
+            this.Sizetxt.Name = "Sizetxt";
+            this.Sizetxt.Size = new System.Drawing.Size(68, 20);
+            this.Sizetxt.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Size :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Solvebtn);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(166, 146);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Initial Information";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 383);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(166, 19);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Solve2btn
+            // 
+            this.Solve2btn.Location = new System.Drawing.Point(12, 248);
+            this.Solve2btn.Name = "Solve2btn";
+            this.Solve2btn.Size = new System.Drawing.Size(166, 23);
+            this.Solve2btn.TabIndex = 5;
+            this.Solve2btn.Text = "Inputs Can be Solved With";
+            this.Solve2btn.UseVisualStyleBackColor = true;
+            this.Solve2btn.Click += new System.EventHandler(this.Solve2btn_Click);
+            // 
+            // Thinklbl
+            // 
+            this.Thinklbl.AutoSize = true;
+            this.Thinklbl.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold);
+            this.Thinklbl.Location = new System.Drawing.Point(47, 300);
+            this.Thinklbl.Name = "Thinklbl";
+            this.Thinklbl.Size = new System.Drawing.Size(0, 29);
+            this.Thinklbl.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 414);
+            this.Controls.Add(this.Thinklbl);
+            this.Controls.Add(this.Solve2btn);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Sizetxt);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Algorithem";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Solvebtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox Sizetxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button Solve2btn;
+        private System.Windows.Forms.Label Thinklbl;
+    }
+}
+
---
+++ KnightAlgorithem/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace KnightAlgorithem
+{
+    public partial class Form1 : Form
+    {
+        const int KMS = 100;
+        const int To_Be_Visited = -1;
+        const int Max_Allowed_Move = 8;
+        static int[,] Bored;
+        static int BoredSize;
+        static int[] Allowed_Move_col = {2,1,-1,-2,-2,-1,1,2 };
+        static int[] Allowed_Move_row = {1,2,2,1,-1,-2,-2,-1 };
+        int InitialCol, InitialRow;
+        Graphics g;
+        Pen p = new Pen(Color.Black);
+        Pen p1 = new Pen(Color.Red);
+        static bool[,] bored2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void PrintBored()
+        {
+            g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            int DrawRow = panel1.Size.Height / BoredSize;
+            int DrawCol = panel1.Size.Width / BoredSize;
+            for (int i = 0; i <= panel1.Size.Height; i+=DrawRow)
+            {
+                g.DrawLine(p, 0, i, panel1.Size.Width, i);
+            }
+            for (int j = 0; j <= panel1.Size.Width; j += DrawCol)
+            {
+                g.DrawLine(p, j, 0, j, panel1.Size.Height);
+            }
+            int DrawC=0,DrawR=0;
+            for (int k = 0; k < BoredSize; k++)
+            {
+                for (int s = 0; s < BoredSize; s++)
+                {
+                    g.DrawString(Bored[k, s].ToString(), new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold), p.Brush, DrawC + DrawCol / 2-4, DrawR + DrawRow / 2-4);
+                    DrawC += DrawCol;
+                }
+                DrawC = 0;
+                DrawR += DrawRow;
+            }
+        }
+
+        public void PrintBored2()
+        {
+            g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            int DrawRow = panel1.Size.Height / BoredSize;
+            int DrawCol = panel1.Size.Width / BoredSize;
+            for (int i = 0; i <= panel1.Size.Height; i += DrawRow)
+            {
+                g.DrawLine(p, 0, i, panel1.Size.Width, i);
+            }
+            for (int j = 0; j <= panel1.Size.Width; j += DrawCol)
+            {
+                g.DrawLine(p, j, 0, j, panel1.Size.Height);
+            }
+            int DrawC = 0, DrawR = 0;
+            bool check = false;
+            for (int k = 0; k < BoredSize; k++)
+            {
+                for (int s = 0; s < BoredSize; s++)
+                {
+                    if (bored2[k, s])
+                    {
+                        check = true;
+                        g.FillRectangle(p1.Brush, DrawC, DrawR, DrawRow, DrawRow);
+                        g.DrawString(k.ToString()+" "+s.ToString(), new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold), p.Brush, DrawC + DrawCol / 2 - 6, DrawR + DrawRow / 2 - 6);
+                        
+                    }
+                    DrawC += DrawCol;
+                }
+                DrawC = 0;
+                DrawR += DrawRow;
+            }
+            if (!check)
+            {
+                MessageBox.Show("There is No Solution For Such Size!!");
+                g.Clear(Color.White);
+            }
+            
+        }
+
+        
+        private void Solvebtn_Click(object sender, EventArgs e)
+        {
+            if (Sizetxt.Text != "" && textBox1.Text != "" && textBox2.Text != "")
+            {
+                InitialCol = Convert.ToInt32(textBox1.Text);
+                InitialRow = Convert.ToInt32(textBox2.Text);
+                BoredSize = Convert.ToInt32(Sizetxt.Text);
+                Bored = new int[BoredSize + 1, BoredSize + 1];
+                progressBar1.Value = 0;
+                progressBar1.Maximum = BoredSize * BoredSize * 10;
+                for (int i = 0; i < BoredSize; i++)
+                {
+                    for (int j = 0; j < BoredSize; j++)
+                    {
+                        Bored[i, j] = To_Be_Visited;
+                    }
+                }
+                Bored[InitialRow, InitialCol] = 0;
+                if (!Solve(InitialRow, InitialCol, 1))
+                    MessageBox.Show("Problem Can Not Be Solved !");
+                else
+                    PrintBored();
+            }
+            else
+                MessageBox.Show("Some Inputs Are Empty!");
+        }
+
+        public bool Solve(int PreviousRow, int PreviousCol, int Next_Step_no)
+        {
+           for (int i = 0; i < Max_Allowed_Move; i++)
+                {
+                    int col = PreviousCol + Allowed_Move_col[i];
+                    int row = PreviousRow + Allowed_Move_row[i];
+                    if (col < 0 || col > BoredSize) continue;
+                    if (row < 0 || row > BoredSize) continue;
+                    if (Bored[row, col] != To_Be_Visited) continue;
+                    Bored[row, col] = Next_Step_no;
+                    if (Next_Step_no == BoredSize * BoredSize - 1)
+                        return true;
+                    else
+                    {
+                        if (Solve(row, col, Next_Step_no + 1))
+                        {
+                            progressBar1.Increment(10);
+                            return true;
+                        }
+                        else
+                        {
+                            Bored[row, col] = To_Be_Visited;
+                        }
+                    }
+                }
+                return false;
+            
+        }
+
+        private void Solve2btn_Click(object sender, EventArgs e)
+        {
+            if (Sizetxt.Text != "")
+            {
+                BoredSize = Convert.ToInt32(Sizetxt.Text);
+                bored2 = new bool[BoredSize, BoredSize];
+                for (int i = 0; i < BoredSize; i++)
+                {
+                    for (int j = 0; j < BoredSize; j++)
+                    {
+                        bored2[i, j] = false;
+                    }
+                }
+                for (int k = 0; k < BoredSize; k++)
+                {
+                    for (int l = 0; l < BoredSize; l++)
+                    {
+                        Bored = new int[BoredSize + 1, BoredSize + 1];
+                        progressBar1.Value = 0;
+                        progressBar1.Maximum = BoredSize * BoredSize * 10;
+                        for (int i = 0; i < BoredSize; i++)
+                        {
+                            for (int j = 0; j < BoredSize; j++)
+                            {
+                                Bored[i, j] = To_Be_Visited;
+                            }
+                        }
+                        Bored[k, l] = 0;
+                        if (Solve(k, l, 1))
+                            bored2[k, l] = true;
+                    }
+                }
+                PrintBored2();
+            }
+            else
+                MessageBox.Show("Enter The Size Field.");
+        }
+       
+    }
+}
